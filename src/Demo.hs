1aa8477b75cc8ee4ee68b05fcd8b510878af003a
David Baynard <davidbaynard@gmail.com>
2018-11-24 21:30:24 +0000

    Begin to create diagonalization routine

--- src/Demo.hs
+++ src/Demo.hs
@@ -16,14 +16,22 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Demo where
 
 import Data.Kind
 import Data.Proxy
 import Data.Void
 import GHC.TypeLits
+import GHC.Exts (IsList)
 
-import Data.Function
+import Control.Monad.State
+import Control.Monad ((<=<))
+import Data.Bifunctor (bimap)
+import Data.Maybe (maybeToList)
+
+-- import Data.Function
 
 data Code =
     Zero
@@ -44,10 +52,10 @@ class Generic a where
 
 instance Generic Bool where
   type CodeOf Bool =
-    Datatype "Bool"
-      (Plus
-        (Constructor "False" One)
-        (Constructor "True" One)
+    'Datatype "Bool"
+      ('Plus
+        ('Constructor "False" 'One)
+        ('Constructor "True" 'One)
       )
 
   from :: Bool -> Rep Bool
@@ -63,10 +71,10 @@ data List a = Nil | Cons a (List a)
 
 instance Generic (List a) where
   type CodeOf (List a) =
-    Datatype "List"
-      (Plus
-        (Constructor "Nil" One)
-        (Constructor "Cons" (Times (Const a) (Const (List a))))
+    'Datatype "List"
+      ('Plus
+        ('Constructor "Nil" 'One)
+        ('Constructor "Cons" ('Times ('Const a) ('Const (List a))))
       )
 
   from :: List a -> Rep (List a)
@@ -101,37 +109,37 @@ instance MyEnum Bool where
 class GEnum (c :: Code) where
   genum :: [Run c]
 
-instance GEnum Zero where
+instance GEnum 'Zero where
   genum = []
 
-instance GEnum One where
+instance GEnum 'One where
   genum = [ () ]
 
-instance (GEnum c1, GEnum c2) => GEnum (Plus c1 c2) where
+instance (GEnum c1, GEnum c2) => GEnum ('Plus c1 c2) where
   genum =
     map Left (genum @c1) ++ map Right (genum @c2)
 
-instance (GEnum c1, GEnum c2) => GEnum (Times c1 c2) where
+instance (GEnum c1, GEnum c2) => GEnum ('Times c1 c2) where
   genum =
     [ (x, y) | x <- genum @c1, y <- genum @c2 ]
 
-instance MyEnum a => GEnum (Const a) where
+instance MyEnum a => GEnum ('Const a) where
   genum = enum
 
-instance GEnum c => GEnum (Datatype n c) where
+instance GEnum c => GEnum ('Datatype n c) where
   genum = genum @c
 
-instance GEnum c => GEnum (Constructor n c) where
+instance GEnum c => GEnum ('Constructor n c) where
   genum = genum @c
 
 type family Run (c :: Code) :: Type where
-  Run Zero              = Void
-  Run One               = ()
-  Run (Const a)         = a
-  Run (Plus c1 c2)      = Either (Run c1) (Run c2)
-  Run (Times c1 c2)     = (Run c1, Run c2)
-  Run (Datatype _ c)    = Run c
-  Run (Constructor _ c) = Run c
+  Run 'Zero              = Void
+  Run 'One               = ()
+  Run ('Const a)         = a
+  Run ('Plus c1 c2)      = Either (Run c1) (Run c2)
+  Run ('Times c1 c2)     = (Run c1, Run c2)
+  Run ('Datatype _ c)    = Run c
+  Run ('Constructor _ c) = Run c
 
 
 
@@ -150,53 +158,53 @@ type family Run (c :: Code) :: Type where
 class GEq (c :: Code) where
   geq :: Run c -> Run c -> Bool
 
-instance GEq Zero where
+instance GEq 'Zero where
   geq x _ = absurd x
 
-instance GEq One where
+instance GEq 'One where
   geq () () = True
 
-instance (GEq c1, GEq c2) => GEq (Plus c1 c2) where
+instance (GEq c1, GEq c2) => GEq ('Plus c1 c2) where
   geq (Left x1) (Left x2) = geq @c1 x1 x2
   geq (Right y1) (Right y2) = geq @c2 y1 y2
   geq _ _ = False
 
-instance (GEq c1, GEq c2) => GEq (Times c1 c2) where
+instance (GEq c1, GEq c2) => GEq ('Times c1 c2) where
   geq (x1, y1) (x2, y2) =
     geq @c1 x1 x2 && geq @c2 y1 y2
 
-instance Eq a => (GEq (Const a)) where
+instance Eq a => (GEq ('Const a)) where
   geq x y = x == y
 
-instance GEq c => GEq (Datatype n c) where
+instance GEq c => GEq ('Datatype n c) where
   geq = geq @c
 
-instance GEq c => GEq (Constructor n c) where
+instance GEq c => GEq ('Constructor n c) where
   geq = geq @c
 
 class GShow (c :: Code) where
   gshow :: Run c -> String
 
-instance GShow Zero where
+instance GShow 'Zero where
   gshow x = absurd x
 
-instance GShow One where
+instance GShow 'One where
   gshow () = ""
 
-instance (GShow c1, GShow c2) => GShow (Plus c1 c2) where
+instance (GShow c1, GShow c2) => GShow ('Plus c1 c2) where
   gshow (Left x) = gshow @c1 x
   gshow (Right y) = gshow @c2 y
 
-instance (GShow c1, GShow c2) => GShow (Times c1 c2) where
+instance (GShow c1, GShow c2) => GShow ('Times c1 c2) where
   gshow (x, y) = gshow @c1 x ++ " " ++ gshow @c2 y
 
-instance Show a => GShow (Const a) where
+instance Show a => GShow ('Const a) where
   gshow x = "(" ++ show x ++ ")"
 
-instance GShow c => GShow (Datatype n c) where
+instance GShow c => GShow ('Datatype n c) where
   gshow = gshow @c
 
-instance (KnownSymbol n, GShow c) => GShow (Constructor n c) where
+instance (KnownSymbol n, GShow c) => GShow ('Constructor n c) where
   gshow x = symbolVal (Proxy @n) ++ " " ++ gshow @c x
 
 
@@ -208,11 +216,13 @@ rel0 3 = [4]
 rel0 4 = [1]
 rel0 _ = []
 
+rel1 :: Int -> [Int]
 rel1 0 = [0]
 rel1 n
   | even n = [n, n `div` 2]
   | otherwise = [3*n + 1, n]
 
+rel2 :: Int -> [Int]
 rel2 11 = [22]
 rel2 22 = [33]
 rel2 33 = [44]
@@ -221,6 +231,7 @@ rel2 n
   | odd n, n>=1, n<=9 = [1,3,5,7,9]
   | otherwise = [n]
 
+rel3 :: Int -> [Int]
 rel3 i = [i + 1]
 
 trancl :: forall a . Eq a => (a -> [a]) -> [a] -> [a]
@@ -247,3 +258,83 @@ trancl r x0 = go [] x0
      -go5 = tgthr [2,0,3,4,1] [1]
      -go6 = tgthr [2,0,3,4,1] []
      -}
+
+diagonalProduct :: [a] -> [b] -> [(a,b)]
+diagonalProduct [] _ = []
+diagonalProduct _ [] = []
+diagonalProduct (a:as) (b:bs) = (a,b) : merge (diagonalProduct (a:as) bs) (diagonalProduct as (b:bs))
+  where
+    merge :: [c] -> [c] -> [c]
+    merge [] xs = xs
+    merge xs [] = xs
+    merge (x:xs) (y:ys) = x : y : merge xs ys
+
+{-
+ -mergeWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+ -mergeWith f (a:as) (b:bs) = f a b
+ -  where
+ -    nextRight (x:xs) (y:ys) = 
+ -}
+
+newtype RevList a = R [a]
+  deriving stock (Eq, Show)
+  deriving newtype (IsList)
+
+newtype ForList a = F [a]
+  deriving stock (Eq, Show)
+  deriving newtype (IsList)
+
+data Zipper a = Z (RevList a) a (ForList a)
+  deriving stock (Eq, Show)
+
+mkZipper :: [a] -> Maybe (Zipper a)
+mkZipper [] = Nothing
+mkZipper (x:xs) = Just $ Z [] x (F xs)
+
+data Diagonal a b = Diagonal (Zipper a) (Zipper b)
+  deriving stock (Eq, Show)
+infixr 6 `Diagonal`
+
+diagonal :: [a] -> [b] -> [(a,b)]
+diagonal = curry $ runDiag <=< maybeToList . uncurry mkDiagonal
+
+mkDiagonal :: [a] -> [b] -> Maybe (Diagonal a b)
+mkDiagonal = curry $ fmap (uncurry Diagonal) . azip . bimap mkZipper mkZipper
+
+azip :: Applicative f => (f a, f b) -> f (a, b)
+azip = uncurry $ (<*>) . fmap (,)
+{-# INLINE azip #-}
+
+runDiag :: Diagonal a b -> [(a,b)]
+runDiag = fix runDiag' False
+
+runDiag'
+  :: (Bool -> Diagonal a b -> [(a,b)])
+  -> Bool -> Diagonal a b -> [(a,b)]
+runDiag' r b0 d0 = case nextDiag b0 d0 of
+  (pair, Nothing) -> [pair]
+  (_, Just (b1, d1)) -> r b1 d1
+
+type NextDiag a b = ((a,b), Maybe (Bool, Diagonal a b))
+
+nextDiag :: Bool -> Diagonal a b -> NextDiag a b
+-- empty case
+nextDiag _ (Z _ b [] `Diagonal` Z _ y []) = ((b,y), Nothing)
+-- Opposite empties
+-- When there is no forward list on one, drop the focus for the other, where there is only a head-forward list
+nextDiag q@False (Z [] b (F (d:c)) `Diagonal` Z (R (w:x)) y []) = ((b,y), Just (q, (Z [] d (F c) `Diagonal` Z (R x) w (F [y]))))
+nextDiag q@False (Z (R (d:a)) b [] `Diagonal` Z [] y (F (w:z))) = ((b,y), Just (not q, (Z (R a) d (F [b]) `Diagonal` Z [] w (F z))))
+-- part forward empty cases
+nextDiag q (Z (R a) b (F (d:c)) `Diagonal` Z x y []) = ((b,y), Just (q, (Z (R (b:a)) d (F c) `Diagonal` Z x y [])))
+nextDiag q (Z a b [] `Diagonal` Z (R x) y (F (w:z))) = ((b,y), Just (q, (Z a b [] `Diagonal` Z (R (y:x)) w (F z))))
+-- initial case
+-- When there is no reverse list for right, shift the left zipper by one
+-- and fiip the zipper
+nextDiag q@False (Z (R a) b (F (d:c)) `Diagonal` Z [] y z) = ((b,y), Just ((not q), (Z (R (b:a)) d (F c) `Diagonal` Z [] y z)))
+-- When there is no reverse list for left, shift the right zipper by one
+-- and fiip the zipper
+nextDiag q@True (Z [] b c `Diagonal` Z (R x) y (F (w:z))) = ((b,y), Just ((not q), (Z [] b c `Diagonal` Z (R (y:x)) w (F z))))
+-- Run the left zipper forwards and the right zipper backwards
+nextDiag q@False (Z (R a) b (F (d:c)) `Diagonal` Z (R (w:x)) y (F z)) = ((b,y), Just (q, (Z (R (b:a)) d (F c) `Diagonal` Z (R x) w (F (y:z)))))
+-- Run the left zipper backwards and the right zipper forwards
+nextDiag q@True (Z (R (d:a)) b (F c) `Diagonal` Z (R x) y (F (w:z))) = ((b,y), Just (q, (Z (R a) d (F (b:c)) `Diagonal` Z (R (y:x)) w (F z))))
--- test/DemoSpec.hs
+++ test/DemoSpec.hs
@@ -1,12 +1,21 @@
 {-# LANGUAGE NumDecimals #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module DemoSpec (main, spec) where
+module DemoSpec (main, spec, raceTest) where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 import Demo
 import Data.List (sort)
 import Control.Concurrent.Async (race_)
 import Control.Concurrent (threadDelay)
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE (toList)
+import Test.QuickCheck.Instances ()
+import Control.Applicative
 
 main :: IO ()
 main = hspec spec
@@ -24,8 +33,60 @@ spec = do
       sort (trancl rel2 [42,8,9]) `shouldBe` [1,2,3,4,5,6,7,8,9,42]
     it "rel1" $
       sort (trancl rel1 [42,8,9]) `shouldBe` [1,2,4,5,7,8,9,10,11,13,14,16,17,20,21,22,26,28,32,34,40,42,52,64]
-    it "handles non-terminating relations" . raceTest $
-      take 10 (trancl rel3 [0]) `shouldBe` [1,2,3,4,5,6,7,8,9,10]
+    {-
+     -it "handles non-terminating relations" . raceTest $
+     -  take 10 (trancl rel3 [0]) `shouldBe` [1,2,3,4,5,6,7,8,9,10]
+     -}
+
+  describe "Diagonalization" $ do
+    prop "makes the right initial diagonal" $ \as'@((a :: Int) :| as) bs'@((b :: Int) :| bs) -> do
+      mkDiagonal (NE.toList as') (NE.toList bs') === Just (Z [] a (F as) `Diagonal` Z [] b (F bs))
+    it "correct diagonalizations 1×1" $ do
+      diagonal [1..1] [1..1] `shouldBe` [(1 :: Int,1 :: Int)]
+    {-
+     -it "correct diagonalizations 2×2" $ do
+     -  diagonal [1..2] [1..2] `shouldBe` [(1,1),(1,2),(2,1),(2,2)]
+     -it "correct diagonalizations 3×3" $ do
+     -  diagonal [1..3] [1..3] `shouldBe` [(1,1),(1,2),(2,1),(3,1),(2,2),(1,3)]
+     -}
+    it "makes the right specific initial diagonal" $ do
+      mkDiagonal [0..4 :: Int] [0..4 :: Int] `shouldBe` Just (Z [] 0 [1..4] `Diagonal` Z [] 0 [1..4])
+    sequence_ $ testingDiagonalExample <$> ZipList [0..] <*> ZipList ordered <*> ZipList (tail ordered)
+
+testingDiagonalExample :: Int -> (NextDiag Int Int) -> (NextDiag Int Int) -> Spec
+testingDiagonalExample n (_, Just ex) res = it ("Example iteration " <> show n) $
+  uncurry nextDiag ex `shouldBe` res
+testingDiagonalExample _ _ _ = error "Invalid test input"
+
+ordered :: [NextDiag Int Int]
+ordered =
+  [ (undefined, Just (False, Z [] 0 [1..4] `Diagonal` Z [] 0 [1..4]))
+  , ((0,0), Just (True, Z [0] 1 [2..4] `Diagonal` Z [] 0 [1..4]))
+  , ((1,0), Just (True, Z [] 0 [1..4] `Diagonal` Z [0] 1 [2..4]))
+  , ((0,1), Just (False, Z [] 0 [1..4] `Diagonal` Z [1,0] 2 [3..4]))
+  , ((0,2), Just (False, Z [0] 1 [2..4] `Diagonal` Z [0] 1 [2..4]))
+  , ((1,1), Just (False, Z [1,0] 2 [3..4] `Diagonal` Z [] 0 [1..4]))
+  , ((2,0), Just (True, Z [2,1..0] 3 [4] `Diagonal` Z [] 0 [1..4]))
+  , ((3,0), Just (True, Z [1,0] 2 [3..4] `Diagonal` Z [0] 1 [2..4]))
+  , ((2,1), Just (True, Z [0] 1 [2..4] `Diagonal` Z [1,0] 2 [3..4]))
+  , ((1,2), Just (True, Z [] 0 [1..4] `Diagonal` Z [2,1..0] 3 [4]))
+  , ((0,3), Just (False, Z [] 0 [1..4] `Diagonal` Z [3,2..0] 4 []))
+  , ((0,4), Just (False, Z [] 1 [2..4] `Diagonal` Z [2,1..0] 3 [4]))
+  , ((1,3), Just (False, Z [1] 2 [3..4] `Diagonal` Z [1,0] 2 [3..4]))
+  , ((2,2), Just (False, Z [2,1] 3 [4] `Diagonal` Z [0] 1 [2..4]))
+  , ((3,1), Just (False, Z [3,2..1] 4 [] `Diagonal` Z [] 0 [1..4]))
+  , ((4,0), Just (True, Z [3,2..1] 4 [] `Diagonal` Z [] 1 [2..4]))
+  , ((4,1), Just (True, Z [2,1] 3 [4] `Diagonal` Z [1] 2 [3..4]))
+  --, ((3,2), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((2,3), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((1,4), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((2,4), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((3,3), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((4,2), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((4,3), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((3,4), Just (True, Z [3,2..0] 4 [] `Diagonal` Z [] 0 [1..4]))
+  --, ((4,4), Nothing)
+  ]
 
 raceTest :: Expectation -> Expectation
 raceTest = race_ (threadDelay 1e6 *> expectationFailure "Test took too long")
