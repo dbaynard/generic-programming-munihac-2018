ca5e0604b1f8e99fc1e9323a3fc2a73cfcd47899
David Baynard <davidbaynard@gmail.com>
2018-11-28 18:05:18 +0000

    Add Enum instance with generic-deriving

--- src/Record.hs
+++ src/Record.hs
@@ -12,12 +12,17 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Record where
 
 import GHC.Generics (Generic)
 import qualified GHC.Generics as G
 import GHC.TypeLits
+
+import Generics.Deriving.Enum
+import Generics.Deriving.Eq
 
 import Data.Proxy
 import Data.Void
@@ -32,6 +37,25 @@ type instance (:::) Void label = Void
 data Shown
 
 type instance (:::) Shown label = Proxy label
+
+--------------------------------------------------------------------------------
+
+newtype AsEq a = AsEq a
+
+instance (Generic a, GEq' (G.Rep a)) => Eq (AsEq a) where
+  AsEq x == AsEq y = x `geqdefault` y
+
+instance (Generic a, GEq' (G.Rep a)) => GEq (AsEq a) where
+  AsEq x `geq` AsEq y = x `geqdefault` y
+
+newtype AsEnum a = AsEnum a
+
+instance (Generic a, GEnum a, Enum' (G.Rep a)) => GEnum (AsEnum a) where
+  genum = AsEnum <$> genum
+
+instance (Generic a, GEq a, Enum' (G.Rep a)) => Enum (AsEnum a) where
+  toEnum = AsEnum . toEnumDefault
+  fromEnum (AsEnum x) = fromEnumDefault x
 
 --------------------------------------------------------------------------------
 
@@ -56,10 +80,11 @@ instance Show (CustomEnum_ Shown) where
   show (First p) = symbolVal p
   show (Second p) = symbolVal p
 
-instance Enum CustomEnum where
-  toEnum 0 = First undefined
-  toEnum 1 = Second undefined
-  toEnum _ = error "Bad argument"
-  fromEnum (First _) = 0
-  fromEnum (Second _) = 1
+deriving via (AsEq CustomEnum) instance Eq CustomEnum
+deriving via (AsEq CustomEnum) instance GEq CustomEnum
 
+instance GEnum Void where
+  genum = []
+
+deriving via (AsEnum CustomEnum) instance GEnum CustomEnum
+deriving via (AsEnum CustomEnum) instance Enum CustomEnum
