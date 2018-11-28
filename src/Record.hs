4c42b04b6c0366acd6682dd77aea931334e5c91d
David Baynard <davidbaynard@gmail.com>
2018-11-28 16:54:41 +0000

    Add some manual instances

--- src/Record.hs
+++ src/Record.hs
@@ -1,13 +1,51 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE StarIsType #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Record where
 
 import GHC.Generics (Generic)
 import qualified GHC.Generics as G
+import GHC.TypeLits
 
-data CustomEnum
-  = First
-  | Second
-  deriving (Generic)
+import Data.Void
+
+--------------------------------------------------------------------------------
+
+type family (:::) (tag :: k) (label :: Symbol) :: *
+infix 5 :::
+
+type instance (:::) Void label = Void
+
+--------------------------------------------------------------------------------
+
+data CustomEnum_ w
+  = First (w ::: "The first")
+  | Second (w ::: "The second")
+  deriving stock (Generic)
+
+type CustomEnum = CustomEnum_ Void
+
+firsty :: CustomEnum
+firsty = First undefined
+
+instance Show CustomEnum where
+  show (First _) = "First shown"
+  show (Second _) = "Second shown"
+
+instance Enum CustomEnum where
+  toEnum 0 = First undefined
+  toEnum 1 = Second undefined
+  toEnum _ = error "Bad argument"
+  fromEnum (First _) = 0
+  fromEnum (Second _) = 1
 
