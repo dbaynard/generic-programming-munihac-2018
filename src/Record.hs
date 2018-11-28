754655f21fcefe9e093579e6224a871add8d0e1b
David Baynard <davidbaynard@gmail.com>
2018-11-28 19:09:21 +0000

    Move back to generic-deriving variant of GEnum

--- src/Record.hs
+++ src/Record.hs
@@ -25,7 +25,6 @@ import GHC.TypeLits
 import Generics.Deriving.Enum
 import Generics.Deriving.Eq
 
-import Data.List (transpose, findIndex)
 import Data.Proxy
 import Data.Void
 
@@ -43,68 +42,6 @@ type instance (:::) Shown label = Proxy label
 
 --------------------------------------------------------------------------------
 
--- From generic-deriving
-toEnumDef :: (Generic a, MyGEnum' (G.Rep a)) => Int -> a
-toEnumDef i = let l = mygenum'
-                  in if (length l > i)
-                      then G.to (l !! i)
-                       else error "toEnum: invalid index"
-
--- From generic-deriving
-fromEnumDef :: (GEq a, Generic a, MyGEnum' (G.Rep a))
-                => a -> Int
-fromEnumDef x = case geq x `findIndex` (G.to <$> mygenum') of
-      Nothing -> error "fromEnum: no corresponding index"
-      Just i  -> i
-
-class MyGEnum a where
-  mygenum :: [a]
-
-  default mygenum :: (Generic a, MyGEnum' (G.Rep a)) => [a]
-  mygenum = G.to <$> mygenum'
-
-class MyGEnum' f where
-  mygenum' :: [f a]
-
-instance MyGEnum' G.V1 where
-  mygenum' = []
-
-instance MyGEnum' G.U1 where
-  mygenum' = [G.U1]
-
-instance (MyGEnum' a, MyGEnum' b) => MyGEnum' (a G.:+: b) where
-  mygenum' = (G.L1 <$> mygenum' @a) ++ (G.R1 <$> mygenum' @b)
-
-instance (MyGEnum' a, MyGEnum' b) => MyGEnum' (a G.:*: b) where
-  mygenum' = uncurry (G.:*:) <$> mygenum' @a `simpleDiagonal` mygenum' @b
-
-instance MyGEnum c => MyGEnum' (G.K1 i c) where
-  mygenum' = G.K1 <$> mygenum @c
-
-instance MyGEnum' a => MyGEnum' (G.M1 i t a) where
-  mygenum' = G.M1 <$> mygenum' @a
-
-simpleDiagonal :: [a] -> [b] -> [(a,b)]
-simpleDiagonal xs0 ys0 = go [] $ makeCartesian xs0 ys0
-  where
-    makeCartesian xs ys = [[(x,y) | x <- xs] | y <- ys]
-
-    go :: forall c . [[c]] -> [[c]] -> [c]
-    go upper lower =
-        -- generate the upper left half and the diagonal, by sticking on
-        -- the head of each list in upper
-      [ h :: c | h:_ <- upper :: [[c]]] ++ case lower of
-        -- transpose the remaining lists
-        [] -> concat . transpose $ upper'
-        -- Recurse, by sticking on the new full-length list
-        -- Each round shrinks each row in upper' by 1
-        row:lower' -> go (row:upper') lower'
-        where
-          -- take the tails of all the lists in upper
-          upper' :: [[c]] = [ t :: [c] | _:t <- upper :: [[c]] ]
-
---------------------------------------------------------------------------------
-
 newtype AsEq a = AsEq a
 
 instance (Generic a, GEq' (G.Rep a)) => Eq (AsEq a) where
@@ -115,12 +52,12 @@ instance (Generic a, GEq' (G.Rep a)) => GEq (AsEq a) where
 
 newtype AsEnum a = AsEnum a
 
-instance (Generic a, GEq a, MyGEnum' (G.Rep a)) => Enum (AsEnum a) where
-  toEnum = AsEnum . toEnumDef
-  fromEnum (AsEnum x) = fromEnumDef x
+instance (Generic a, GEq a, Enum' (G.Rep a)) => Enum (AsEnum a) where
+  toEnum = AsEnum . toEnumDefault
+  fromEnum (AsEnum x) = fromEnumDefault x
 
-instance (Generic a, GEq a, MyGEnum' (G.Rep a)) => MyGEnum (AsEnum a) where
-  mygenum = AsEnum . G.to <$> mygenum'
+instance (Generic a, GEq a, Enum' (G.Rep a)) => GEnum (AsEnum a) where
+  genum = AsEnum . G.to <$> enum'
 
 --------------------------------------------------------------------------------
 
@@ -148,8 +85,5 @@ instance Show (CustomEnum_ Shown) where
 deriving via (AsEq CustomEnum) instance Eq CustomEnum
 deriving via (AsEq CustomEnum) instance GEq CustomEnum
 
-deriving via (AsEnum Void) instance MyGEnum Void
-deriving via (AsEnum ()) instance MyGEnum ()
-
-deriving via (AsEnum CustomEnum) instance MyGEnum CustomEnum
+deriving via (AsEnum CustomEnum) instance GEnum CustomEnum
 deriving via (AsEnum CustomEnum) instance Enum CustomEnum
